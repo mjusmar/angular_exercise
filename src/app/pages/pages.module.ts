a3e2c988ba07eac09fe998967d802211d7f57636
mjusmar <mjusma@unal.edu.co>
2022-09-12 09:09:15 -0500

    feat: service to change account theme was included and so its component, service to navigate through, main, progressbar and graphs pages in sidebar was included

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -11,6 +11,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graph1Component } from './graph1/graph1/graph1.component';
 import { PagesComponent } from './pages.component';
+import { AccountSettingsComponent } from './account-settings/account-settings.component';
 
 
 @NgModule({
@@ -19,6 +20,7 @@ import { PagesComponent } from './pages.component';
     ProgressComponent,
     Graph1Component,
     PagesComponent,
+    AccountSettingsComponent,
   ],
   exports: [
     DashboardComponent,
